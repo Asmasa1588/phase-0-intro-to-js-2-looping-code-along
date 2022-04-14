199739f23d012ace4141827447a6dd8bb580d110
Asma <as1588ma@gmail.com>
2022-04-13 21:32:23 -0400

    commit tree 4638d74d7f846a0529268851537f7cd9a5f7e6d0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(nameArr, event) {
+  return nameArr.map((name) => {
+    return `Thank you, ${name}, for the wonderful ${event} gift!`;
+  });
+}
+function countDown(number) {
+  let outPut = number;
+  for (let i = 0; i <= number; i++) {
+    console.log(outPut);
+    // outPut--;
+    outPut = outPut - 1;
+  }
+}
